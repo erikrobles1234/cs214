a81df2b0b30fa01e7ab6e018a0337cae149a687d
Erik Robles <50894791+erikrobles1234@users.noreply.github.com>
2021-09-16 12:16:34 -0400

    Update factor.c

--- factor.c
+++ factor.c
@@ -1,17 +1,26 @@
-int 
+bool checkPrime(int n) {
+	bool isPrime = true;
+	for (j = 2; j <= n/2; ++j) {
+		if (n % j == 0) {
+			isPrime = false;
+			break;
+		}
+	}
+	return isPrime;
+}
 
 int main() {
 	printf("Please enter a number greater than 2: ");
 	printf("/n");
 	int scanf("%d", n);
+
 	bool isPrime = true;
 
 	for (i = 2; i <= n/2; ++i) {
-		for (j = i; j <= n/2; ++j) {
-			if (n % i == 0) {
-				isPrime = false;
-				break;
-			}
+		isPrime = checkPrime(i);
+		if (isPrime) {
+			printf("%d", i);
+			printf(" ");
 		}
 	}
 
